ea9ea371e7a5e430170b0cb2a2ffa6720165c2ca
fluzzi <fluzzi@gmail.com>
2020-05-18 16:47:06 -0300

    add install

--- setup.py
+++ setup.py
@@ -5,29 +5,60 @@ import fileinput
 import shutil
 from shutil import copyfile
 import os
+import re
+import stat
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 workdir = os.path.dirname(os.path.realpath(__file__))
 js = open('servers.json')
 servers = json.loads(js.read())
 keys = list(servers.keys())
 allfiles = ["supervisord.conf","rootfs/vpn.sh"]
-homekey = os.path.expanduser("~") + "/.ssh/id_rsa.pub"
+home = os.path.expanduser("~")
+homekey = home + "/.ssh/id_rsa.pub"
+paths = os.environ['PATH'].split(os.pathsep)
+pathpattern1 = re.compile("^" + home  +"/bin$")
+pathpattern2 = re.compile("^" + home  +"/.local/bin$")
+bindir = None
+for i in paths:
+    if pathpattern1.match(i) or pathpattern2.match(i):
+        bindir = i
+if bindir == None:
+    print(f"{bcolors.WARNING}Warning: add {home}/bin to PATH env{bcolors.ENDC}")
+    bindir = home + "/bin"
+scriptfile = bindir + "/vpn"
 
-if not os.path.exists('compose'):
-    os.makedirs('compose')
+if not os.path.exists(homekey):
+    print(f"{bcolors.FAIL}Please run ss-keygen to generate a ssh key{bcolors.ENDC}")
+    exit()
+else:
+    copyfile(homekey,"authorized_keys")
+
+if not os.path.exists(bindir + '/compose'):
+    os.makedirs(bindir + '/compose')
 if not os.path.exists('config'):
     os.makedirs('config')
 if not os.path.exists('log'):
     os.makedirs('log')
 
-if not os.path.exists(homekey):
-    print("Please run ssh-keygen to generate a ssh key")
-    exit()
-else:
-    copyfile(homekey,"authorized_keys")
-
 def make_files(vpnid):
-    vpnname = "compose/vpn." + vpnid + ".yml"
+    vpnname = bindir + "/compose/vpn." + vpnid + ".yml"
+    copyfile("vpn",scriptfile)
+    st = os.stat(scriptfile)
+    os.chmod(scriptfile, st.st_mode | stat.S_IEXEC)
+    with fileinput.FileInput(scriptfile, inplace = True) as script:
+        for line in script:
+            newline = line.replace("workdir = None", "workdir = '" + workdir + "'")
+            print(newline.replace("bindir = None", "bindir = '" + bindir + "'"), end='')
     copyfile("vpn.yml", vpnname)
     workfiles = allfiles.copy()
     workfiles.append(vpnname)
