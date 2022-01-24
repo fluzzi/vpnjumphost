f2798e8015bf411e91bb878990db6e66581bf723
fluzzi32 <fluzzi32@Norman.lan>
2022-01-24 15:34:01 -0500

    update

--- rootfs/vpn.sh
+++ rootfs/vpn.sh
@@ -5,4 +5,4 @@ ifconfig tun1 up
 # start vpn
 source /vpn/config/config.vpnid
 openconnect -s /etc/vpnc/vpnc-script $server $options --user=$user --interface=tun1 --passwd-on-stdin < /vpn/config/pass.vpnid.txt
-kill -9 1
+kill 1
